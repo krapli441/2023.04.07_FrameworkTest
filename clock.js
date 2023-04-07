f1fe1d3715029ca25d1406aa80048cf28e0a3621
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-07 10:15:02 +0900

    시간 생성 확인

---
+++ clock.js
@@ -0,0 +1,8 @@
+const time = new Date();
+console.log(time.getFullYear());
+console.log(time.getMonth() + 1);
+console.log(time.getDate());
+console.log(time.getHours());
+console.log(time.getMinutes());
+
+console.log(time);
