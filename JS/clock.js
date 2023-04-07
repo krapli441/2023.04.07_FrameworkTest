694f39bdbb1fbbdb1b5272d5f94c1b4d9c93e2e1
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-07 10:56:56 +0900

    요일 추가

--- JS/clock.js
+++ JS/clock.js
@@ -1,15 +1,26 @@
 const clock = document.querySelector("#clock");
 
+let weekend = new Array(7);
+weekend[0] = "Sunday";
+weekend[1] = "Monday";
+weekend[2] = "Tuesday";
+weekend[3] = "Wednesday";
+weekend[4] = "Thursday";
+weekend[5] = "Friday";
+weekend[6] = "Saturday";
+
 function getTime() {
   const time = new Date();
   const years = String(time.getFullYear());
   const month = String(time.getMonth() + 1);
   const date = String(time.getDate());
+  const day = weekend[time.getDay()];
   const hours = String(time.getHours()).padStart(2, "0");
   const minutes = String(time.getMinutes()).padStart(2, "0");
   const seconds = String(time.getSeconds()).padStart(2, "0");
   clock.innerText = `${years}.${month}.${date}
-  ${hours}:${minutes}:${seconds}`;
+  ${hours}:${minutes}:${seconds}
+  ${day}`;
 }
 
 getTime();
